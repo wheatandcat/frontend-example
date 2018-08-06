50e61cda70f943f02b657557f9c36111fcc84d17
“wheatandcat” <wheatandcat@gmail.com>
2018-08-06 23:22:24 +0900

    add form

--- nuxtjs-exapmles/003-form/pages/index.vue
+++ nuxtjs-exapmles/003-form/pages/index.vue
@@ -1,65 +1,110 @@
 <template>
   <section class="container">
     <div>
-      <app-logo/>
-      <h1 class="title">
-        003-form
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+      <h1>form</h1>
+      <br />
+          名前:
+      <input v-model="input.name" placeholder="name"/>
+      <br />
+      <br />
+      性別:
+      <input type="radio" id="male" value="1" v-model="input.genderCode" />
+      <label for="male" style="padding-right:8px">男性</label>
+      <input type="radio" id="female" value="2" v-model="input.genderCode" />
+      <label for="female">女性</label>
+      <br />
+      <br />
+      <button v-on:click="save">登録</button>
+      <br />
+      <br />
+      <h1>users</h1>
+      <table border="1" style="width:30rem">
+        <tr>
+          <th>id</th>
+          <th>名前</th>
+          <th>性別</th>
+          <th>action</th>
+        </tr>
+        <tr v-for="user in $store.state.users" :key="user.id">
+          <td>{{user.id}}</td>
+          <td>{{user.name}}</td>
+          <td>{{user.genderCode == "1" ? "男性" : "女性"}}</td>
+          <td>
+            <button v-on:click="remove(user.id)">削除</button>
+          </td>
+        </tr>
+      </table>
     </div>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+import axios from "axios";
 
 export default {
-  components: {
-    AppLogo
+  data() {
+    return {
+      input: {
+        name: "",
+        genderCode: "1"
+      }
+    };
+  },
+  async fetch({ store, params }) {
+    let { data } = await axios.get("http://localhost:3000/users");
+
+    store.commit("setUsers", data);
+  },
+  methods: {
+    getItems: async function() {
+      let { data } = await axios.get("http://localhost:3000/users");
+
+      this.$store.commit("setUsers", data);
+    },
+    save: async function() {
+      const response = await fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(this.input)
+      });
+
+      if (!response.ok) {
+        return alert("通信エラー");
+      }
+
+      this.input = {
+        name: "",
+        genderCode: "1"
+      };
+
+      // リストを再取得
+      this.getItems();
+    },
+    remove: async function(id) {
+      const response = await fetch(`http://localhost:3000/users/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+
+      if (!response.ok) {
+        return alert("削除に失敗しました");
+      }
+
+      // リストを再取得
+      this.getItems();
+    }
   }
-}
+};
 </script>
 
 <style>
 .container {
   min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  padding: 2rem;
 }
 </style>
 
