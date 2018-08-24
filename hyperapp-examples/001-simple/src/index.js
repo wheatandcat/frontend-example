0d75f456678c09be4eef50a1841a621c7cd196cc
wheatandcat <wheatandcat@gmail.com>
2018-08-25 05:25:22 +0900

    add *

--- hyperapp-examples/001-simple/src/index.js
+++ hyperapp-examples/001-simple/src/index.js
@@ -1,24 +1,14 @@
 import { h, app } from "hyperapp";
 
-const state = {
-  count: 0
-};
+const state = {};
 
-const actions = {
-  down: () => state => ({ count: state.count - 1 }),
-  up: () => state => ({ count: state.count + 1 })
-};
+const actions = {};
 
-const view = (state, actions) => (
+const view = () => (
   <main style={{ padding: "1rem" }}>
     <h3>hyper-app | 001-simple</h3>
     <br />
-    <div>
-      <span style={{ paddingLeft: "1rem" }}>{state.count}</span>
-      <br />
-      <button onclick={actions.down}>-</button>
-      <button onclick={actions.up}>+</button>
-    </div>
+    <div>welcome</div>
   </main>
 );
 
