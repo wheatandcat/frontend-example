6328e35a385165d99442b36941d245de595e2427
wheatandcat <wheatandcat@gmail.com>
2018-08-26 13:47:46 +0900

    update

--- nuxtjs-exapmles/001-simple/pages/index.vue
+++ nuxtjs-exapmles/001-simple/pages/index.vue
@@ -1,13 +1,13 @@
 <template>
   <section class="container">
     <div>
-      <app-logo/>
-      <h1 class="title">
+      <h3>
         001-simple
-      </h1>
-      <h2 class="subtitle">
+      </h3>
+      <br />
+      <p>
         Nuxt.js project
-      </h2>
+      </p>
     </div>
   </section>
 </template>
@@ -24,29 +24,7 @@ export default {
 
 <style>
 .container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+  padding: 2rem;
 }
 
 .links {
