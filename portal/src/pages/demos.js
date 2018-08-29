89b24c474630079e7063b7f23b96a578f5b95f87
wheatandcat <wheatandcat@gmail.com>
2018-08-30 01:01:00 +0900

    add

--- portal/src/pages/demos.js
+++ portal/src/pages/demos.js
@@ -1,219 +1,168 @@
-const demos = [
-  {
+const demos = [{
     name: "hyperapp",
-    items: [
-      {
+    items: [{
         name: "001-simple",
-        demo:
-          "https://wheatandcat.github.io/examples-pages/frontend-example/hyperapp-examples/001-simple/index.html",
-        source:
-          "https://github.com/wheatandcat/frontend-example/tree/master/hyperapp-examples/001-simple"
+        demo: "https://wheatandcat.github.io/examples-pages/frontend-example/hyperapp-examples/001-simple/index.html",
+        source: "https://github.com/wheatandcat/frontend-example/tree/master/hyperapp-examples/001-simple"
       },
       {
         name: "002-fetch",
-        demo:
-          "https://wheatandcat.github.io/examples-pages/frontend-example/hyperapp-examples/002-fetch/index.html",
-        source:
-          "https://github.com/wheatandcat/frontend-example/tree/master/hyperapp-examples/002-fetch"
+        demo: "https://wheatandcat.github.io/examples-pages/frontend-example/hyperapp-examples/002-fetch/index.html",
+        source: "https://github.com/wheatandcat/frontend-example/tree/master/hyperapp-examples/002-fetch"
       },
       {
         name: "003-form",
-        demo:
-          "https://wheatandcat.github.io/examples-pages/frontend-example/hyperapp-examples/003-form/index.html",
-        source:
-          "https://github.com/wheatandcat/frontend-example/tree/master/hyperapp-examples/003-form"
+        demo: "https://wheatandcat.github.io/examples-pages/frontend-example/hyperapp-examples/003-form/index.html",
+        source: "https://github.com/wheatandcat/frontend-example/tree/master/hyperapp-examples/003-form"
       },
       {
         name: "004-router",
         demo: "https://hyperapp-router.herokuapp.com/users",
-        source:
-          "https://github.com/wheatandcat/frontend-example/tree/master/hyperapp-examples/004-router"
+        source: "https://github.com/wheatandcat/frontend-example/tree/master/hyperapp-examples/004-router"
       }
     ]
   },
   {
     name: "elm",
-    items: [
-      {
+    items: [{
         name: "001-simple",
-        demo:
-          "https://wheatandcat.github.io/examples-pages/frontend-example/elm-examples/001-simple/index.html",
-        source:
-          "https://github.com/wheatandcat/frontend-example/tree/master/elm-examples/001-simple"
+        demo: "https://wheatandcat.github.io/examples-pages/frontend-example/elm-examples/001-simple/index.html",
+        source: "https://github.com/wheatandcat/frontend-example/tree/master/elm-examples/001-simple"
       },
       {
         name: "002-fetch",
-        demo:
-          "https://wheatandcat.github.io/examples-pages/frontend-example/elm-examples/002-fetch/index.html",
-        source:
-          "https://github.com/wheatandcat/frontend-example/tree/master/elm-examples/002-fetch"
+        demo: "https://wheatandcat.github.io/examples-pages/frontend-example/elm-examples/002-fetch/index.html",
+        source: "https://github.com/wheatandcat/frontend-example/tree/master/elm-examples/002-fetch"
       },
       {
         name: "003-form",
-        demo:
-          "https://wheatandcat.github.io/examples-pages/frontend-example/elm-examples/003-form/index.html",
-        source:
-          "https://github.com/wheatandcat/frontend-example/tree/master/elm-examples/003-form"
+        demo: "https://wheatandcat.github.io/examples-pages/frontend-example/elm-examples/003-form/index.html",
+        source: "https://github.com/wheatandcat/frontend-example/tree/master/elm-examples/003-form"
       },
       {
         name: "004-router",
-        demo:
-          "https://wheatandcat.github.io/examples-pages/frontend-example/elm-examples/004-router/index.html",
-        source:
-          "https://github.com/wheatandcat/frontend-example/tree/master/elm-examples/004-router"
+        demo: "https://wheatandcat.github.io/examples-pages/frontend-example/elm-examples/004-router/index.html",
+        source: "https://github.com/wheatandcat/frontend-example/tree/master/elm-examples/004-router"
       }
     ]
   },
   {
     name: "preact",
-    items: [
-      {
+    items: [{
         name: "001-simple",
-        demo:
-          "https://wheatandcat.github.io/examples-pages/frontend-example/preact-examples/001-simple/index.html",
-        source:
-          "https://github.com/wheatandcat/frontend-example/tree/master/preact-examples/001-simple"
+        demo: "https://wheatandcat.github.io/examples-pages/frontend-example/preact-examples/001-simple/index.html",
+        source: "https://github.com/wheatandcat/frontend-example/tree/master/preact-examples/001-simple"
       },
       {
         name: "002-fetch",
-        demo:
-          "https://wheatandcat.github.io/examples-pages/frontend-example/preact-examples/002-fetch/index.html",
-        source:
-          "https://github.com/wheatandcat/frontend-example/tree/master/preact-examples/002-fetch"
+        demo: "https://wheatandcat.github.io/examples-pages/frontend-example/preact-examples/002-fetch/index.html",
+        source: "https://github.com/wheatandcat/frontend-example/tree/master/preact-examples/002-fetch"
       },
       {
         name: "003-form",
-        demo:
-          "https://wheatandcat.github.io/examples-pages/frontend-example/preact-examples/003-form/index.html",
-        source:
-          "https://github.com/wheatandcat/frontend-example/tree/master/preact-examples/003-form"
+        demo: "https://wheatandcat.github.io/examples-pages/frontend-example/preact-examples/003-form/index.html",
+        source: "https://github.com/wheatandcat/frontend-example/tree/master/preact-examples/003-form"
       },
       {
         name: "004-router",
         demo: "https://preact-router.herokuapp.com/",
-        source:
-          "https://github.com/wheatandcat/frontend-example/tree/master/preact-examples/004-router"
+        source: "https://github.com/wheatandcat/frontend-example/tree/master/preact-examples/004-router"
       }
     ]
   },
   {
     name: "vuejs",
-    items: [
-      {
+    items: [{
         name: "001-simple",
-        demo:
-          "https://wheatandcat.github.io/examples-pages/frontend-example/preact-examples/001-simple/index.html",
-        source:
-          "https://github.com/wheatandcat/frontend-example/tree/master/vue-examples/001-simple"
+        demo: "https://wheatandcat.github.io/examples-pages/frontend-example/vue-examples/001-simple/index.html",
+        source: "https://github.com/wheatandcat/frontend-example/tree/master/vue-examples/001-simple"
       },
       {
         name: "002-fetch",
-        demo:
-          "https://wheatandcat.github.io/examples-pages/frontend-example/preact-examples/002-fetch/index.html",
-        source:
-          "https://github.com/wheatandcat/frontend-example/tree/master/vue-examples/002-fetch"
+        demo: "https://wheatandcat.github.io/examples-pages/frontend-example/vue-examples/002-fetch/index.html",
+        source: "https://github.com/wheatandcat/frontend-example/tree/master/vue-examples/002-fetch"
       },
       {
         name: "003-form",
-        demo:
-          "https://wheatandcat.github.io/examples-pages/frontend-example/preact-examples/003-form/index.html",
-        source:
-          "https://github.com/wheatandcat/frontend-example/tree/master/vue-examples/003-form"
+        demo: "https://wheatandcat.github.io/examples-pages/frontend-example/vue-examples/003-form/index.html",
+        source: "https://github.com/wheatandcat/frontend-example/tree/master/vue-examples/003-form"
       },
       {
         name: "004-router",
-        demo: "https://high-lan.glitch.me/",
-        source:
-          "https://github.com/wheatandcat/frontend-example/tree/master/vue-examples/004-router"
+        demo: "https://vue-router.herokuapp.com/#/users",
+        source: "https://github.com/wheatandcat/frontend-example/tree/master/vue-examples/004-router"
       }
     ]
   },
   {
     name: "nuxtjs",
-    items: [
-      {
+    items: [{
         name: "001-simple",
         demo: "https://nuxtjs-simple.herokuapp.com/",
-        source:
-          "https://github.com/wheatandcat/frontend-example/tree/master/nuxtjs-exapmles/001-simple"
+        source: "https://github.com/wheatandcat/frontend-example/tree/master/nuxtjs-exapmles/001-simple"
       },
       {
         name: "002-fetch",
         demo: "https://nuxtjs-fetch.herokuapp.com/",
-        source:
-          "https://github.com/wheatandcat/frontend-example/tree/master/nuxtjs-exapmles/002-fetch"
+        source: "https://github.com/wheatandcat/frontend-example/tree/master/nuxtjs-exapmles/002-fetch"
       },
       {
         name: "003-form",
         demo: "https://nuxtjs-form.herokuapp.com/",
-        source:
-          "https://github.com/wheatandcat/frontend-example/tree/master/nuxtjs-exapmles/003-form"
+        source: "https://github.com/wheatandcat/frontend-example/tree/master/nuxtjs-exapmles/003-form"
       },
       {
         name: "004-router",
-        demo: "https://high-lan.glitch.me/",
-        source: "https://nuxtjs-router.herokuapp.com/"
+        demo: "https://nuxtjs-router.herokuapp.com/",
+        source: "https://github.com/wheatandcat/frontend-example/tree/master/nuxtjs-exapmles/004-router"
       }
     ]
   },
   {
     name: "nextjs",
-    items: [
-      {
+    items: [{
         name: "001-simple",
         demo: "https://nextjs-simple.herokuapp.com/",
-        source:
-          "https://github.com/wheatandcat/frontend-example/tree/master/nextjs-examples/001-simple"
+        source: "https://github.com/wheatandcat/frontend-example/tree/master/nextjs-examples/001-simple"
       },
       {
         name: "002-fetch",
         demo: "https://nextjs-fetch.herokuapp.com/",
-        source:
-          "https://github.com/wheatandcat/frontend-example/tree/master/nextjs-examples/002-fetch"
+        source: "https://github.com/wheatandcat/frontend-example/tree/master/nextjs-examples/002-fetch"
       },
       {
         name: "003-form",
         demo: "https://nextjs-form.herokuapp.com/",
-        source:
-          "https://github.com/wheatandcat/frontend-example/tree/master/nextjs-examples/003-form"
+        source: "https://github.com/wheatandcat/frontend-example/tree/master/nextjs-examples/003-form"
       },
       {
         name: "004-router",
         demo: "https://nextjs-router.herokuapp.com/",
-        source:
-          "https://github.com/wheatandcat/frontend-example/tree/master/nextjs-examples/004-router"
+        source: "https://github.com/wheatandcat/frontend-example/tree/master/nextjs-examples/004-router"
       }
     ]
   },
   {
     name: "angular",
-    items: [
-      {
+    items: [{
         name: "001-simple",
-        demo:
-          "https://wheatandcat.github.io/examples-pages/frontend-example/preact-examples/001-simple/index.html",
-        source:
-          "https://github.com/wheatandcat/frontend-example/tree/master/angular-examples/001-simple"
+        demo: "https://wheatandcat.github.io/examples-pages/frontend-example/preact-examples/001-simple/index.html",
+        source: "https://github.com/wheatandcat/frontend-example/tree/master/angular-examples/001-simple"
       },
       {
         name: "002-fetch",
-        demo:
-          "https://wheatandcat.github.io/examples-pages/frontend-example/preact-examples/002-fetch/index.html",
-        source:
-          "https://github.com/wheatandcat/frontend-example/tree/master/angular-examples/002-fetch"
+        demo: "https://wheatandcat.github.io/examples-pages/frontend-example/preact-examples/002-fetch/index.html",
+        source: "https://github.com/wheatandcat/frontend-example/tree/master/angular-examples/002-fetch"
       },
       {
         name: "003-form",
-        demo:
-          "https://wheatandcat.github.io/examples-pages/frontend-example/preact-examples/003-form/index.html",
-        source:
-          "https://github.com/wheatandcat/frontend-example/tree/master/angular-examples/003-form"
+        demo: "https://wheatandcat.github.io/examples-pages/frontend-example/preact-examples/003-form/index.html",
+        source: "https://github.com/wheatandcat/frontend-example/tree/master/angular-examples/003-form"
       },
       {
         name: "004-router",
         demo: "https://high-lan.glitch.me/",
-        source:
-          "https://github.com/wheatandcat/frontend-example/tree/master/angular-examples/004-router"
+        source: "https://github.com/wheatandcat/frontend-example/tree/master/angular-examples/004-router"
       }
     ]
   }
