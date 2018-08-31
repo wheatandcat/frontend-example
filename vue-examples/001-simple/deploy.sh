989d2d6470569f96004ad23785ee5cd6e0500617
wheatandcat <wheatandcat@gmail.com>
2018-08-31 23:41:29 +0900

    add report

--- vue-examples/001-simple/deploy.sh
+++ vue-examples/001-simple/deploy.sh
@@ -11,3 +11,4 @@ cp -R dist/* ../../../examples-pages/frontend-example/${ROOT_DIR}/${DIR}
 sed -i -e "s/src=/\src=\./g" ../../../examples-pages/frontend-example/${ROOT_DIR}/${DIR}/index.html
 sed -i -e "s/href=/\href=\./g" ../../../examples-pages/frontend-example/${ROOT_DIR}/${DIR}/index.html
 open ../../../examples-pages/frontend-example/${ROOT_DIR}/${DIR}/index.html
+
---
+++ vue-examples/001-simple/report.sh
@@ -0,0 +1,15 @@
+#!/bin/bas
+MOCK_HOST=https://mock-server-yznxmkzmvo.now.sh
+ROOT_DIR=vue-examples
+DIR=001-simple
+
+rm -rf build/*
+VUE_APP_HOST=${MOCK_HOST} npm run build -- --report
+rm -rf ../../../examples-pages/frontend-example/${ROOT_DIR}/${DIR}/report
+mkdir -p ../../../examples-pages/frontend-example/${ROOT_DIR}/${DIR}/report
+cp dist/report.html ../../../examples-pages/frontend-example/${ROOT_DIR}/${DIR}/report
+
+serve -s dist &
+lighthouse http://localhost:5000 --output json --output-path ../../../examples-pages/frontend-example/${ROOT_DIR}/${DIR}/report/report.json
+kill $(jobs -p)
+open ../../../examples-pages/frontend-example/${ROOT_DIR}/${DIR}/report
